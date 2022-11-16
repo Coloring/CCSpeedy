b9fd3510a0ace6a71e957da03374226346b4cb3e
coloring <coloring>
2022-11-16 12:00:52 +0800

    shadow

---
+++ CCSpeedy/Classes/CCShadow.swift
@@ -0,0 +1,21 @@
+
+import Foundation
+
+public extension CALayer {
+    
+    func skt_setShadow(color: UIColor? = .black,
+                       alpha: CGFloat = 0.5,
+                       x: CGFloat = 0, y: CGFloat = 2,
+                       blur: CGFloat = 4,
+                       spread: CGFloat = 0) {
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur * 0.5
+        shadowColor = color?.cgColor
+        shadowOpacity = Float(alpha)
+
+        let rect = bounds.insetBy(dx: -spread, dy: -spread)
+        let path = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius)
+        shadowPath = path.cgPath
+    }
+    
+}
