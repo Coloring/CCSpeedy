339ee9590136f6afa36a201dca800824598fb060
minghui5.zhu <minghui5.zhu@tcl.com>
2023-07-10 19:58:32 +0800

    update

--- CCSpeedy/Classes/CCTextField.swift
+++ CCSpeedy/Classes/CCTextField.swift
@@ -5,13 +5,32 @@ import UIKit
 extension UITextField {
  
     /// 添加左内边距
+    public func addLeftRightTextPadding(_ blankSize: CGFloat) {
+        let leftView = UIView()
+        leftView.frame = CGRect(x: 0, y: 0, width: blankSize, height: frame.height)
+        self.leftView = leftView
+        self.leftViewMode = UITextField.ViewMode.always
+        
+        let rightView = UIView()
+        rightView.frame = CGRect(x: 0, y: 0, width: blankSize, height: frame.height)
+        self.rightView = rightView
+        self.rightViewMode = UITextField.ViewMode.always
+    }
+    
     public func addLeftTextPadding(_ blankSize: CGFloat) {
         let leftView = UIView()
         leftView.frame = CGRect(x: 0, y: 0, width: blankSize, height: frame.height)
         self.leftView = leftView
         self.leftViewMode = UITextField.ViewMode.always
     }
- 
+    
+    public func addRightTextPadding(_ blankSize: CGFloat) {
+        let rightView = UIView()
+        rightView.frame = CGRect(x: 0, y: 0, width: blankSize, height: frame.height)
+        self.rightView = rightView
+        self.rightViewMode = UITextField.ViewMode.always
+    }
+     
     /// 在文本框的左边添加一个图标
     public func addLeftIcon(_ image: UIImage?, frame: CGRect, imageSize: CGSize) {
         let leftView = UIView()
